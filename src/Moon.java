541608832dc00757a91fbb134a9cbea8b1841acd
PetrenkoMS <el-2002@mail.ru>
2022-01-31 21:56:28 +0300

    Fix Bug

--- src/Moon.java
+++ src/Moon.java
@@ -1,5 +1,5 @@
 public class Moon {
     public static void main(String[] args) {
-        System.out.println("Mooooooooooon! EEEEEEEEEEEE!");
+        System.out.println("Mooooooooooon!");
     }
 }
