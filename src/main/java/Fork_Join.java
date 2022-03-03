6ac98d6b55daf574e38e89eacc464cf00e820039
PetrenkoMS <el-2002@mail.ru>
2022-03-03 21:20:33 +0300

    Add Fork/Join + Fork/Join Action

--- src/main/java/Fork_Join.java
+++ src/main/java/Fork_Join.java
@@ -1,7 +1,53 @@
 package main.java;
 
-public class Fork_Join {
-    public static void main(String[] args) {
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
 
+public class Fork_Join {
+    //Факториал
+    public static void main(String[] args) {
+        ForkJoinPool pool = new ForkJoinPool();
+        CountFactTask task = new CountFactTask(15);
+        System.out.println("Result : " + pool.invoke(task));
     }
+}
+
+class CountFactTask extends RecursiveTask<Long> {
+
+    private  static  final long serialVersionUID = 1L;
+    private long num1;
+    private long num2;
+    public CountFactTask(long num1, long num2) {
+        super();
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+    public CountFactTask(long number) {
+        this(1,number);
+    }
+
+    @Override
+    protected Long compute() { //простая ли задача, или надо разбивать?
+       if(num1 - num2 <=5) {
+           return multiply(num2);
+       }
+       else {
+           long mid = num1 + (num2-num1)/2;
+           CountFactTask task1 = new CountFactTask(num1, mid);
+           CountFactTask task2 = new CountFactTask(mid+1,num2);
+           task1.fork(); //откладывает для рассчетов позже
+           long c = task2.compute(); //fork -> compute -> join --- иначе нельзя! зависнет
+           return c*task1.join();
+
+       }
+    }
+
+    private long multiply(long num) {
+        if (num == num1) {
+            return num1;
+        }
+        return num * multiply(num-1);
+    }
+
+
 }
---
+++ src/main/java/Fork_join_action.java
@@ -0,0 +1,90 @@
+package main.java;
+
+import java.util.Locale;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+import java.util.concurrent.RecursiveTask;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Fork_join_action {
+    public static final int ARRAY_LENGTH = 50;
+
+    public static void main(String[] args) {
+        ForkJoinPool pool = new ForkJoinPool();
+        String[] strings = new String[ARRAY_LENGTH];
+        for (int i = 0; i < ARRAY_LENGTH; i++) {
+            strings[i] = createRandomString();
+        }
+        printStringTable(strings);
+        UpperCaseAction action = new UpperCaseAction(strings);
+        pool.invoke(action);
+        printStringTable(strings);
+    }
+
+    public static String createRandomString() {
+        String letters = "qazwsxedcrfvtgbyhnujmikolp";
+        int stringlength = ThreadLocalRandom.current().nextInt(3, 21);
+        String generatedString = "";
+        for (int i = 0; i < stringlength; i++) {
+            generatedString += letters.charAt(ThreadLocalRandom.current().nextInt(letters.length()));
+        }
+        return generatedString;
+    }
+
+    public static void printStringTable(String[] strings) {
+        for (int i = 0; i < ARRAY_LENGTH; i++) {
+            System.out.println(strings[i] + " ");
+            if (i % 5 == 0 && i != 0) {
+                System.out.println();
+            }
+        }
+        System.out.println();
+    }
+}
+
+class UpperCaseAction extends RecursiveAction {
+    private static final long serialVersionUID = 1L;
+    private String [] strings;
+    private int start;
+    private int end;
+    private int size;
+
+    public UpperCaseAction(String[] strings, int start, int end) {
+        super();
+        this.strings = strings;
+        this.start = start;
+        this.end = end;
+        this.size = size;
+    }
+
+    public UpperCaseAction(String[] strings) {
+        this(strings, 0, strings.length);
+    }
+
+    @Override
+    protected void compute() {
+        if (size<=5) {
+            tableToUpperCase();
+        }
+        else {
+            int mid=start+size/2;
+            UpperCaseAction action1 = new UpperCaseAction(strings,start,mid);
+            UpperCaseAction action2 = new UpperCaseAction(strings, mid+1, end);
+
+//            action1.fork();
+//            action2.compute();
+//            action1.join();
+            invokeAll(action1,action2); //Можно, т.к. RecursiveAction ничего не возвращает
+        }
+
+    }
+
+    private void tableToUpperCase() {
+        for (int i = start; i < end; i++) {
+            strings[i]=strings[i].toUpperCase();
+        }
+    }
+
+}
+
+
